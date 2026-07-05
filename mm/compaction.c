2d10c8d7da25e3f34e6a8c1a0c5ae0aa2bf9db79
de1sek <lzhenyang702@gmail.com>
2026-07-05 13:25:11 +0800

    Update compaction.c

--- mm/compaction.c
+++ mm/compaction.c
@@ -2503,7 +2503,9 @@ static void compact_nodes(void)
 		compact_node(nid);
 }
 
-void zram_compact(void);
+    #ifdef CONFIG_ZRAM
+    void zram_compact(void);
+    #endif
 
 static void do_compaction(struct work_struct *work)
 {
@@ -2516,8 +2518,10 @@ static void do_compaction(struct work_struct *work)
 	/* Do full compaction */
 	compact_nodes();
 
+	#ifdef CONFIG_ZRAM
 	/* Do ZRAM compaction */
 	zram_compact();
+    #endif
 
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
